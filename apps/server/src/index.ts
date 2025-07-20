f54df957fdd7fc80ef58a86ae50688e37b3a628d
Agastya Gaur <agastyagaur17@gmail.com>
2025-07-20 11:07:16 +0530

    init project

---
+++ apps/server/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import {logger} from '@repo/logger'; // Adjust the import path as necessary
+const app = express();
+app.use(express.json());
+
+
+
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const [method, url, status, responseTime] = message.split(" ");
+        const formattedLog = `${method} - ${url} - ${status} - ${responseTime?.trim()}ms`;
+        logger.info(formattedLog);
+      },
+    },
+  })
+);
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
